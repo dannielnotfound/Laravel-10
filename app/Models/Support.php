952e30104ddb7f90da9f5834dab1b73841571b82
dannielnotfound <danielriber548@gmail.com>
2023-06-04 19:03:05 -0300

    feat: enums

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -2,16 +2,27 @@
 
 namespace App\Models;
 
+use App\Enums\SupportStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
-{
+{   
+    use HasFactory;
+
+
     protected $fillable = [
         "subject",
         "body",
         "status"
     ];
 
-    use HasFactory;
+    # Mutators & Acessors
+    public function status(): Attribute
+    {
+        return Attribute::make(
+            set: fn (SupportStatus $status) => $status->name,  
+        );
+    }
 }
