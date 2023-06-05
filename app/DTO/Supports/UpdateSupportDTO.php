e2e5d6f6452dade4d8ae01b600ad903e196b5fe8
dannielnotfound <danielriber548@gmail.com>
2023-06-04 23:33:46 -0300

    feat: api rest methods

--- app/DTO/Supports/UpdateSupportDTO.php
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -14,13 +14,13 @@ class UpdateSupportDTO
         public string $body,
     ){}
 
-    public static function makeFromRequest(StoreUpdateSupport $request): self
+    public static function makeFromRequest(StoreUpdateSupport $request, string $id = null): self
     {
         return new self(
-            $request->id,
+            $id ?? $request->id,
             $request->subject,
             SupportStatus::A,
-            $request->body,
+            $request->body
         );
     }
 }
